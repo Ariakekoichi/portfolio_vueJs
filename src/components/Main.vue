d9a12a70d83078a1d5916abfdfcc5ccb58908ba4
LAPTOP-KM9VP3E2\jipai <jipai1121@gmail.com>
2022-03-07 17:50:22 +0900

    포폴사이트 번역수정

--- src/components/Main.vue
+++ src/components/Main.vue
@@ -1,71 +1,75 @@
 <template>
     <div class="hero">
-        <img class="background" src="../assets/img/background.svg" />
-        <div class="hero__container">
-        <div class="hero__main">
-            <nav>
-                Some pages are not yet translated.
-                <ul>
-                    <li><a href="#" v-on:click.prevent="changeLanguage('en')">English</a></li>
-                    <li><a href="#" v-on:click.prevent="changeLanguage('ja')">日本語</a></li>
-                    <li><a href="#" v-on:click.prevent="changeLanguage('cn')">简体中文</a></li>
-                    <li><a href="#" v-on:click.prevent="changeLanguage('tw')">繁体中文</a></li>
-                    <li><a href="#" v-on:click.prevent="changeLanguage('kr')">한국어</a></li>
+        <img class="background" src="@/assets/img/background.svg" />
+        <div class="container">
+            <div class="main">
+                <nav class="text-right">
+                    <ul class="list-inline">
+                        <li class="list-inline-item"><a href="#" v-on:click.prevent="changeLanguage('en')">English</a></li>
+                        <li class="list-inline-item"><a href="#" v-on:click.prevent="changeLanguage('ja')">日本語</a></li>
+                        <li class="list-inline-item"><a href="#" v-on:click.prevent="changeLanguage('cn')">简体中文</a></li>
+                        <li class="list-inline-item"><a href="#" v-on:click.prevent="changeLanguage('tw')">繁体中文</a></li>
+                        <li class="list-inline-item"><a href="#" v-on:click.prevent="changeLanguage('kr')">한국어</a></li>
+                    </ul>
+                    ※ Some pages are not translated yet.
+                </nav>
+                <h5 class="title">{{ $t("hyunheeLee") }}</h5>
+                <h3>{{ $t("Software Developer") }}</h3>
+                <ul class="links">
+                    <li>
+                        <a
+                        class="link"
+                        href="https://github.com/Ariakekoichi/"
+                        target="__blank"
+                        title="Hyunhee Lee GitHub(in preparation)"
+                        >Github</a
+                        >
+                    </li>
+                    <li>
+                        <a
+                        class="link"
+                        href="https://www.linkedin.com/in/hyunhee1121/"
+                        target="__blank"
+                        title="Hyunhee Lee LinkedIn"
+                        >LinkedIn</a
+                        >
+                    </li>
+                    <li>
+                        <a
+                        class="link"
+                        href="https://calendly.com/jipai1121"
+                        target="__blank"
+                        title="Hyunhee Lee Schedule a call"
+                        >Schedule a call</a
+                        >
+                    </li>
                 </ul>
-            </nav>
-            <h5 class="hero__title">{{ $t("hyunheeLee") }}</h5>
-            <h3 class="hero__sub-title">{{ $t("Software Developer") }}</h3>
-
-            <a
-            class="hero__btn"
-            href="mailto:jipai1121@gmail.com"
-            subject="subject text"
-            target="__blank"
-            title="Hyunhee Lee e-mail"
-            >Message Me</a
-            >
-            About
-            {{ $t("About") }}
-        </div><!-- hero__main -->
-        <div class="hero__footer">
-            <li class="hero__links">
-            <ul>
                 <a
-                class="hero__link"
-                href="https://github.com/Ariakekoichi/"
-                target="__blank"
-                title="Hyunhee Lee GitHub(in preparation)"
-                >Github</a
+                    class="btn"
+                    href="mailto:jipai1121@gmail.com"
+                    subject="subject text"
+                    target="__blank"
+                    title="Hyunhee Lee e-mail"
+                    >Message Me</a
                 >
-            </ul>
-            <ul>
-                <a
-                class="hero__link"
-                href="https://www.linkedin.com/in/hyunhee1121/"
-                target="__blank"
-                title="Hyunhee Lee LinkedIn"
-                >LinkedIn</a
-                >
-            </ul>
-            <ul>
-                <a
-                class="hero__link"
-                href="https://calendly.com/jipai1121"
-                target="__blank"
-                title="Hyunhee Lee Schedule a call"
-                >Schedule a call</a
-                >
-            </ul>
-            </li>
-            <div class="hero__copyright">© 2022 Hyunhee Lee made by <a
-                class="hero__link"
-                href="https://vuejs.org/"
-                target="__blank"
-                title="Vue.js"
-                >Vue.js</a
-                ></div>
-            </div>
-        </div><!-- hero__footer -->
+                <h2 class="sub-title">About</h2>
+                {{ $t("About") }}
+                <h2 class="sub-title">Career</h2>
+                <h2 class="sub-title">Education</h2>
+                {{ $t("Education") }}
+                <h2 class="sub-title">Personal Activities</h2>
+            </div><!-- main -->
+            <div class="footer">
+                <div class="copyright">© 2022 Hyunhee Lee made by <a
+                    class="link"
+                    href="https://vuejs.org/"
+                    target="__blank"
+                    title="Vue.js"
+                    >Vue.js</a
+                    >
+                </div>
+            </div><!-- footer -->
+        </div>
     </div>
 </template>
 
@@ -88,7 +92,7 @@ export default {
   height: 100%;
   object-fit: cover;
 }
-.hero__container {
+.container {
   display: flex;
   justify-content: space-between;
   flex-direction: column;
@@ -98,8 +102,8 @@ export default {
   margin: 0px auto;
   padding: 0px 20px 0px 20px;
 }
-.hero__main {
-  max-width: 678px;
+.main {
+  /*max-width: 678px;*/
   width: 100%;
   margin: 0 auto;
   padding-top: 70px;
@@ -108,18 +112,20 @@ export default {
   text-align: center;
   align-items: center;
 }
-.hero__title {
+.title {
   font-size: 72px;
   font-weight: 900;
   line-height: 91px;
 }
-.hero__sub-title {
+.sub-title {
   font-size: 36px;
   font-weight: 400;
   line-height: 45px;
   padding-top: 10px;
+  border-bottom: 5px solid #444;
+  margin-bottom: 15px;
 }
-.hero__btn {
+.btn {
   display: flex;
   align-items: center;
   justify-content: center;
@@ -131,11 +137,11 @@ export default {
   font-size: 14px;
   margin: 48px auto 0px;
 }
-.hero__btn:hover {
+.btn:hover {
   opacity: 0.8;
   transition: 0.5s;
 }
-.hero__footer {
+.footer {
   display: flex;
   flex-direction: column;
   justify-content: space-between;
@@ -143,7 +149,7 @@ export default {
   max-height: 200px;
   height: 100%;
 }
-.hero__links {
+.links {
   display: flex;
   margin: auto;
   max-width: 536px;
@@ -153,52 +159,52 @@ export default {
   line-height: 18px;
   font-weight: 400;
 }
-.hero__link:hover {
+.link:hover {
   transition: 0s;
   font-weight: 700;
 }
-.hero__copyright {
+.copyright {
   font-size: 15px;
   margin: auto;
   margin: 0 auto;
 }
 @media (max-width: 707px) {
-  .hero__main {
+  .main {
     padding-top: 50px;
   }
 }
 @media (max-width: 555px) {
-  .hero__main {
+  .main {
     padding-top: 40px;
   }
-  .hero__title {
+  .title {
     line-height: 70px;
   }
-  .hero__title {
+  .title {
     font-size: 65px;
     line-height: 60px;
   }
-  .hero__sub-title {
+  .sub-title {
     font-size: 26px;
   }
-  .hero__btn {
+  .btn {
     margin: 40px auto 70px;
   }
 }
 @media (max-width: 480px) {
-  .hero__title {
+  .title {
     font-size: 45px;
     line-height: 40px;
   }
-  .hero__btn {
+  .btn {
     margin-bottom: 40px;
   }
-  .hero__links {
+  .links {
     flex-direction: column;
     align-items: center;
     gap: 15px;
   }
-  .hero__copyright {
+  .copyright {
     font-size: 8px;
     margin-top: 25px;
   }
